005f5ef54a0c013a80137785d8c27b79108efaeb
ynikulyak <45584946+ynikulyak@users.noreply.github.com>
2019-07-01 23:58:10 -0700

    Update recovery.php

--- recovery.php
+++ recovery.php
@@ -34,17 +34,18 @@
 			<div class="text-panel">
 				<h1>Viruses Prevention</h1>
 				<p> <img class="virus-image" src="img/antivirus.png" alt="antivirus">
-				<ul id="prevention"> 
+					<ul id="prevention"> 
 						<li class="bullet">Use antivirus software.</li>
 						<li class="bullet">Do not work under privileged accounts unless absolutely necessary.</li>
 						<li class="bullet">Do not run unfamiliar programs from dubious sources.</li>
 						<li class="bullet">Try to block the possibility of unauthorized changes to system files.</li>
-    					<li class="bullet">Disable potentially dangerous system functionality.</li>
+    						<li class="bullet">Disable potentially dangerous system functionality.</li>
 						<li class="bullet">Do not go to suspicious sites, pay attention to the address in the address bar of the browser.</li>
-    					<li class="bullet">Use only trusted distributions.</li>
+    						<li class="bullet">Use only trusted distributions.</li>
 						<li class="bullet">Constantly make backup copies of important data and have an image of the system with all the settings for fast deployment.</li>
 						<li class="bullet">Perform regular updates of frequently used programs, especially those that provide system security.</li>
-				</ul> </p>
+					</ul>
+				</p>
 
 				<a class="read-again" href="index.php">Back to the beginning</a>
 				Information source can be seen <a href="https://en.wikipedia.org/wiki/Computer_virus#Operations_and_functions">here</a>.
