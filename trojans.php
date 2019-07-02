48b6efb5fbab714e7cd9216f94130fae9c138aab
ynikulyak <45584946+ynikulyak@users.noreply.github.com>
2019-07-01 23:40:22 -0700

    Update trojans.php

--- trojans.php
+++ trojans.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>
 		Computer Viruses
 	</title>
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Monoton|Nunito&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
+
 </head>
 <body>
 	<div class="main-layout">
@@ -31,7 +33,7 @@
 
 		<div class="main-panel">
 			<div class="text-panel">
-				<p id="trojan"> <img class="virus-image" src="img/trojan.png" alr="trojan-virus"> 
+				<p id="trojan"> <img class="virus-image" src="img/trojan.png" alt="trojan-virus"> 
 				 <span class="virus-type">Trojans horses</span><br>
 					This name comes from a software which is very useful software but later do damage you system after run once. This virus can track the login details of the users who is online. Example, E-banking is very common, so its vulnerability of tracing your login details whenever your PC is working without the support of strong antivirus.</p>
 				
